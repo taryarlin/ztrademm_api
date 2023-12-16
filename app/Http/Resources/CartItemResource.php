c5c23be6b852a6589fe982af8f7594e79dd98f01
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2023-12-16 14:45:35 +0630

    [update] Cart item API

--- app/Http/Resources/CartItemResource.php
+++ app/Http/Resources/CartItemResource.php
@@ -9,11 +9,24 @@ class CartItemResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request                                        $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $this->load('product', 'product.Category', 'product.SubCategory', 'product.ProductImage');
+
+        return [
+            'id' => $this->id,
+            'session_key' => $this->session_key,
+            'user_id' => $this->user_id,
+            'product_id' => $this->product_id,
+            'quantity' => $this->quantity,
+            'single_price' => $this->single_price,
+            'total_price' => $this->total_price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'product' => new ProductResource($this->product),
+        ];
     }
 }
