a4bdf5704537a01a13a25ddf89c3d44045466608
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2023-12-17 17:06:52 +0630

    Update ProductImage.php

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -8,11 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImage extends Model
 {
     use HasFactory;
-    protected $fillable =[
+
+    protected $fillable = [
         'thumbnails',
         'product_id'
     ];
-    public function Product(){
-        return $this->belongsTo(Product::class,'product_id');
+
+    protected $appends = ['full_thumbnail_link'];
+
+    public function Product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function getFullThumbnailLinkAttribute()
+    {
+        $image_name = str_replace('"', '', $this->attributes['thumbnails']);
+
+        return "https://api.ztrademm.com/storage/product_image/" . $image_name;
     }
 }
